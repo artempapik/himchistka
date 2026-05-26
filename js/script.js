5b309690b94521f9db60b05762c2c4146bf53a28
artempapik <51436664+artempapik@users.noreply.github.com>
2026-05-26 17:16:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const loadPage = async (e, name) => {
+const loadPage = async (e, page) => {
     const button = e.target.closest('button')
     if (button.classList.contains('active')) {
         return
@@ -7,7 +7,7 @@ const loadPage = async (e, name) => {
     document.querySelectorAll('nav button').forEach(b => b.classList.remove('active'))
     button.classList.add('active')
 
-    const result = await fetch(name + '.html')
+    const result = await fetch(`html/${page}.html`)
     const text = await result.text()
     document.querySelector('main').innerHTML = text
 }
